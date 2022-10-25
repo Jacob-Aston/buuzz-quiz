3ff2973d00ff5366826695122dd56229feb9d9be
unknown <mwgoodwin2000@gmail.com>
2022-10-24 19:13:21 -0600

    added api to js file

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 var requestUrl = 'https://api.github.com/orgs/nodejs/repos?per_page=5';
+var pokeUrl = 'https://pokeapi.co/api/v2/pokemon/ditto'
+var starUrl = 'https://swapi.dev/api/'
+
 var responseText = document.getElementById('response-text');
-function getApi(requestUrl) {
-  fetch(requestUrl)
+
+function getApi(pokeUrl) {
+  fetch(pokeUrl)
     .then(function (response) {
       console.log(response);
       if (response.status === 200) {
@@ -10,4 +14,17 @@ function getApi(requestUrl) {
       return response.json();
   });
 }
-getApi(requestUrl);
+getApi(pokeUrl);
+
+function getApi(starUrl) {
+  fetch(starUrl)
+    .then(function (response) {
+      console.log(response);
+      if (response.status === 200) {
+        responseText.textContent = response.status;
+      }
+      return response.json();
+  });
+}
+getApi(starUrl);
+
