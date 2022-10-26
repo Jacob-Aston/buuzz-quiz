7859b16d2a421b018d13fb5b462eef2a259bbed2
PaigeWing <paige_wing@aol.com>
2022-10-25 19:33:20 -0600

    started JS

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
 var requestUrl = 'https://api.github.com/orgs/nodejs/repos?per_page=5';
 var pokeUrl = 'https://pokeapi.co/api/v2/pokemon/ditto'
 var starUrl = 'https://swapi.dev/api/'
+var answerButtons = document.getElementById('answer-buttons')
+var questionText = document.getElementById('question')
 
 var responseText = document.getElementById('response-text');
+
+var currentQuestion;
 
 function getApi(pokeUrl) {
   fetch(pokeUrl)
@@ -27,6 +31,23 @@ function getApi(starUrl) {
   });
 }
 getApi(starUrl);
+
+
+function startQuiz(question) {
+  currentQuestion = 0
+  questionText.textContent = question.question
+  questions.answers.forEach(answer => {
+  var button = document.createElement("button")
+  button.innerText = answer.text
+  button.classList.add("btn")
+  button.addEventListener('click', selectAnswer)
+  answerButtons.appendChild(button)
+  console.log("response");
+  })
+}
+startQuiz();
+
+function selectAnswer()
 
 const questions = [{
 question: "When you're bored at night do you?",
