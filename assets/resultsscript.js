edd6f5b42398f70bbe78ed841dfd42d40b6b2c8a
RainbowToad <chad_spilker@hotmail.com>
2022-11-01 12:03:45 -0700

    cleaning up codes and comments on results.js

--- assets/resultsscript.js
+++ assets/resultsscript.js
@@ -2,16 +2,13 @@ var reveal = document.querySelector("#btn");
 var result = document.getElementById("result");
 var quiz2 = document.getElementById("quiz2");
 
-
-
-
 var title = document.getElementById("title1");
 var type = document.getElementById("type");
 var sprite = document.getElementById("img");
 var finalCharacter = localStorage.getItem("result")
 
 
-
+// function to fetch from pokemon API
 const getPokemon = () => {
     var pokeUrl = `https://pokeapi.co/api/v2/pokemon/${finalCharacter}`;
     fetch(pokeUrl)
@@ -23,6 +20,7 @@ const getPokemon = () => {
         type.textContent = "#" + data.id;
         document.getElementById("img").src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${finalCharacter}.png`
         
+        //Utilize local storage to keep record of previous results and store new result
         var existing = localStorage.getItem(`character`);
         existing = existing ? JSON.parse(existing) : [];
         existing.push(data.name);
@@ -34,9 +32,7 @@ const getPokemon = () => {
 
 
 
-
-
-
+// function to fetch from disney API
 const getDisney = () => {
     var disneyUrl = `https://api.disneyapi.dev/characters/${finalCharacter}`;
     fetch(disneyUrl)
@@ -49,6 +45,8 @@ const getDisney = () => {
         type.textContent = data.films;
         document.getElementById("img").src = data.imageUrl;
         document.getElementById("info").href = `https://disney.fandom.com/wiki/${data.name}`;
+        
+        //Utilize local storage to keep record of previous results and store new result
         var existing = localStorage.getItem(`character`);
         existing = existing ? JSON.parse(existing) : [];
         existing.push(data.name);
@@ -56,6 +54,7 @@ const getDisney = () => {
     })    
 };
 
+// On click run appropriate function and display results
 reveal.addEventListener("click", function(){
     result.style.display = "block";
     reveal.style.display = "none";
